d867b0782a1658f38fddf5e4d657c31ccc8c5b2b
J. Patricio Parada <paradagpatricio@gmail.com>
2023-07-13 19:03:44 -0400

    Initial commit. Basic template

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+const propertiesJSON = [
+    {
+      name: "Country House",
+      description: "A perfect place to run out from the city",
+      src:
+        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+      rooms: 2,
+      surface: 170
+    },
+    {
+      name: "Beach House",
+      description: "Wake up with the sound of the waves.",
+      src:
+        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+      rooms: 2,
+      surface: 130
+    },
+    {
+      name: "Downtown House",
+      description: "Live in the heart of the city.",
+      src:
+        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+      rooms: 1,
+      surface: 80
+    },
+    {
+      name: "Trailer",
+      description: "A house that goes with you.",
+      src:
+        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+      rooms: 1,
+      surface: 6
+    },
+    {
+      name: "Apartment",
+      description: "A perfect view of the city.",
+      src:
+        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+      rooms: 3,
+      surface: 200
+    },
+    {
+      name: "Mansion",
+      description: "Luxury and elegance in one place.",
+      src:
+        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+      rooms: 5,
+      surface: 500
+    }
+  ];

7514bfe986d41ef3ebea2b3afd406e121adf7013
Patricio Parada <paradagpatricio@gmail.com>
2023-07-20 00:14:53 -0400

    Update script file Add all the needed logic for the buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,3 +48,93 @@ const propertiesJSON = [
       surface: 500
     }
   ];
+
+function createProperty(property){
+    // first we need a div container for out property
+    let prop_div = document.createElement('div');
+    // adding the class
+    prop_div.classList.add('property');
+    // adding needed elements to show the property card
+    prop_div.innerHTML = `
+    <div class="prop-img"
+      style="background-image: url('${property.src}')">
+    </div>
+    <section>
+      <h5>${property.name}</h5>
+      <div class="dflex justify-content-between">
+        <p>Rooms: ${property.rooms}</p>
+        <p>Surface (m): ${property.surface}</p>
+      </div>
+      <p class="my-3">${property.description}</p>
+      <button class="btn btn-info">See more</button>
+    </section>
+    `;
+    // returning the node
+    return prop_div;
+}
+
+function defaultView() {
+    let total = propertiesJSON.length;
+    total_properties.innerText = total;
+    // we need to clean the properties div
+    while (properties_div.firstChild) properties_div.removeChild(properties_div.firstChild);
+
+    propertiesJSON.forEach(property => {
+        let prop_div = createProperty(property);
+        properties_div.appendChild(prop_div);
+    });
+    // ensure that input labels are clear
+    document.getElementById("rooms").value = '';
+    document.getElementById("surf-min").value = '';
+    document.getElementById("surf-max").value = '';
+}
+
+// some parent elements, to make my life easier
+const total_properties = document.getElementById('total-properties');
+const properties_div = document.getElementsByClassName('properties')[0];
+
+// generating default view
+defaultView();
+
+// here the magic comes true
+document.getElementById("filter-btn").addEventListener('click', () => {
+    // get values to filter
+    const rooms = parseInt(document.getElementById("rooms").value);
+    const surf_min = parseInt(document.getElementById("surf-min").value);
+    const surf_max = parseInt(document.getElementById("surf-max").value);
+
+    // check if the user pass all the needed inputs
+    if (!rooms && !surf_min && !surf_max) {
+        alert('Missing input.');
+        return;
+    }
+
+    // empty array to store all matching properties
+    const properties = [];
+    
+    // loop over the properties array
+    propertiesJSON.forEach(obj => {
+        // check if property match the user conditions and add it to the array when match
+        if ((obj.rooms >= rooms) && (obj.surface >= surf_min) && (obj.surface <= surf_max)) {
+            properties.push(obj);
+        }
+    });
+    
+    // if we have at least one match
+    if (properties.length) {
+        // we need to clean the properties div
+        while (properties_div.firstChild) properties_div.removeChild(properties_div.firstChild);
+        properties.forEach(property => {
+            // here we're creating the property container
+            let prop_div = createProperty(property);
+            properties_div.appendChild(prop_div);
+        });
+        total = properties.length;
+        total_properties.innerText = total;
+    } else {
+        defaultView();
+        alert('No matches for your search');
+    }
+});
+
+document.getElementById('reset-btn').addEventListener('click', () => defaultView());
